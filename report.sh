9863f3bea9569fb59a7170a198928e553559a998
smz <shimizu.michinao@gmail.com>
2022-07-09 22:27:15 +0900

    init

--- report.sh
+++ report.sh
@@ -17,7 +17,7 @@ repo_name=$1
 declare repo_total=0
 
 echo '## Workflows'
-echo "${repo_name} における今月の Workflow 別総実行時間です。"
+echo "${repo_name} における今月の Workflow 別総実行時間"
 echo '| workflow(status) | workflow(source) | id | state | billable time |'
 echo '| ---------------- | ---------------- | -- | ----- | ------------- |'
 while IFS="|" read -r id name html_url state badge_url path; do
@@ -30,13 +30,13 @@ while IFS="|" read -r id name html_url state badge_url path; do
     echo "| [![$name]($badge_url)](/$repo_name/actions/workflows/${path##*/}) | [$name]($html_url) | $id | $state | $(humanize $total) |"
 done < <(gh api /repos/$repo_name/actions/workflows --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"')
 
-echo "## Repository Total"
-echo "${repo_name} における今月の総実行時間です。"
-echo "__$(humanize $repo_total)__"
+echo '### Note'
+echo '- Enterpriseプランでは __50000分/月__ が上限で、それ以上は追加購入が必要です。'
+echo '- _timeout-minutes_ を指定しないジョブは最大 __3600分__ 中断されません。'
+echo '- _timeout-minutes_ を指定したジョブは指定した時間で中断されます。'
+echo '- _jobs_ あるいは _steps_ に _timeout-minutes_ を指定してください。'
 
-echo '## Message'
-echo '- 全体で __50000分/月__ が現在の上限です。'
-echo '- _timeout-minutes_ を指定しないジョブは最大 __6時間(3600分)__ 実行される可能性があります。'
-echo '- _timeout-minutes_ を指定したジョブは最大でも指定した時間で終了します。'
-echo '- _jobs_ あるいは _steps_ に _timeout-minutes_ を指定しましょう。'
+echo "## Repository Total"
+echo "${repo_name} における今月の Workflows 総実行時間"
+echo "__$(humanize $repo_total)__"
 exit 0
