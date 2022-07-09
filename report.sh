09d031f82052c4a45f83b889d637d5acea3fc2e0
smz <shimizu.michinao@gmail.com>
2022-07-09 20:56:32 +0900

    init

--- report.sh
+++ report.sh
@@ -16,9 +16,9 @@ humanize() {
 repo_name=$1
 declare repo_total=0
 
-echo '## Billable Time'
-echo '| badge | id | name/source | state | billable time total |'
-echo '| ----- | -- | ----------- | ----- | ------------------- |'
+echo '## Billable Time（Workflows）'
+echo '| status badge | id | name/source | state | billable time |'
+echo '| ------------ | -- | ----------- | ----- | ------------- |'
 while IFS="|" read -r id name html_url state badge_url path; do
     total=0
     for ms in $(gh api "/repos/${repo_name}/actions/workflows/$id/timing" --jq ".billable[].total_ms")
@@ -29,6 +29,6 @@ while IFS="|" read -r id name html_url state badge_url path; do
     echo "| [![$name]($badge_url)](/$repo_name/actions/workflows/${path##*/}) | $id | [$name]($html_url) | $state | $(humanize $total) |"
 done < <(gh api /repos/$repo_name/actions/workflows --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"')
 
-echo "## Total Billable Time"
+echo "## Billable Time（Repository Total）"
 echo "__$(humanize $repo_total)__"
 exit 0
