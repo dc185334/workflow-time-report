50e081b63fbf9b995812b10c3aead5f0a722d201
smz <shimizu.michinao@gmail.com>
2022-07-09 21:24:04 +0900

    init

--- report.sh
+++ report.sh
@@ -18,8 +18,8 @@ declare repo_total=0
 
 echo '## Billable Time'
 echo '### Workflows'
-echo '| status badge | id | name/source | state | billable time |'
-echo '| ------------ | -- | ----------- | ----- | ------------- |'
+echo '| workflow(status) | id | workflow(source) | state | billable time |'
+echo '| ---------------- | -- | ---------------- | ----- | ------------- |'
 while IFS="|" read -r id name html_url state badge_url path; do
     total=0
     for ms in $(gh api "/repos/${repo_name}/actions/workflows/$id/timing" --jq ".billable[].total_ms")
@@ -36,5 +36,5 @@ echo "__$(humanize $repo_total)__"
 echo '## Message'
 echo '- _timeout-minutes_ を指定しないジョブは最大 __6時間（3600分）__ 実行されてしまいます。'
 echo '- _timeout-minutes_ を指定たジョブ指定した時間で終了します。'
-echo '- ワークフローを記載する際 jobs には _timeout-minutes_ を指定してください。'
+echo '- ワークフローを記載する際 _jobs_ には _timeout-minutes_ を指定してください。'
 exit 0
