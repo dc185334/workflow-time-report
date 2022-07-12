a431628397ad2dd1fe98aa36887e803a23fbd59b
smz <shimizu.michinao@gmail.com>
2022-07-12 22:42:40 +0900

    refactor

--- report.sh
+++ report.sh
@@ -2,7 +2,9 @@
 
 set -puo pipefail
 
-humanize() {
+declare repo_total=0
+
+millsec_humanize() {
     local millsec=$1
     if (( millsec < 1000 )); then
         printf "%s ms" $millsec
@@ -13,19 +15,23 @@ humanize() {
     fi
 }
 
-declare repo_total=0
+print_table_header {
+    echo '| workflow id | status badge | name/source | state | total billable time |'
+    echo '| ----------- | ------------ | ----------- | ----- | ------------------- |'
+}
 
-echo '| workflow id | status badge | name/source | state | total billable time |'
-echo '| ----------- | ------------ | ----------- | ----- | ------------------- |'
+print_table_rows {
+    while IFS="|" read -r id name html_url state badge_url path; do
+        local total=0
+        for ms in $(gh api "/repos/$1}/actions/workflows/$id/timing" --jq ".billable[].total_ms")
+        do
+            total=$(( total + ms ))
+            repo_total=$(( repo_total + ms ))
+        done
+        echo "| $id | [![$name]($badge_url)](/$1/actions/workflows/${path##*/}) | [$name]($html_url) | $state | $(millsec_humanize $total) |"
+    done < <(gh api "/repos/$1/actions/workflows" --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"')
+    echo "| | | | | __$(millsec_humanize $repo_total)__ |"
+}
 
-while IFS="|" read -r id name html_url state badge_url path; do
-    total=0
-    for ms in $(gh api "/repos/${TARGET_REPOSITORY}/actions/workflows/$id/timing" --jq ".billable[].total_ms")
-    do
-        total=$(( total + ms ))
-        repo_total=$(( repo_total + ms ))
-    done
-    echo "| $id | [![$name]($badge_url)](/$TARGET_REPOSITORY/actions/workflows/${path##*/}) | [$name]($html_url) | $state | $(humanize $total) |"
-done < <(gh api "/repos/$TARGET_REPOSITORY/actions/workflows" --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"')
-
-echo "| | | | | __$(humanize $repo_total)__ |"
+print_table_header
+print_table_rows $TARGET_REPOSITORY
