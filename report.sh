aec967f8c1a5e892e55c6c6a440d40db95bc90e1
smz <shimizu.michinao@gmail.com>
2022-07-12 23:50:25 +0900

    test

--- report.sh
+++ report.sh
@@ -13,26 +13,16 @@ humanize() {
     fi
 }
 
-get_workflows() {
-    gh api "/repos/$1/actions/workflows" --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"'
-}
-
-get_workflow() {
-    gh api "/repos/${repo}/actions/workflows/$id/timing" --jq ".billable[].total_ms"
-}
-
 main() {
     local repo=$1
+    local total_time=0
     echo '| workflow id | status badge | name/source | state | total billable time |'
     echo '| ----------- | ------------ | ----------- | ----- | ------------------- |'
     while IFS="|" read -r id name html_url state badge_url path; do
-        local total_time=0
-        for workflow_time in $(get_workflow $repo)
-        do
-            total_time=$(( total_time + workflow_time ))
-        done
-        echo "| $id | [![$name]($badge_url)](/$repo/actions/workflows/${path##*/}) | [$name]($html_url) | $state | $(humanize $total_time) |"
-    done < <(get_workflows $repo)
+        local workflow_time=$(gh api "/repos/${repo}/actions/workflows/$id/timing" --jq ".billable[].total_ms")
+        total_time=$(( total_time + workflow_time ))
+        echo "| $id | [![$name]($badge_url)](/$repo/actions/workflows/${path##*/}) | [$name]($html_url) | $state | $(humanize $workflow_time) |"
+    done < <(gh api "/repos/$repo/actions/workflows" --jq '.workflows[] | "\(.id)|\(.name)|\(.html_url)|\(.state)|\(.badge_url)|\(.path)"')
     echo "| | | | | $(humanize $total_time) |"
 }
 main "$TARGET_REPOSITORY"
